1212ae0512b36ca92f4988bb472b71bb28fc7938
Clarissa Staudt <45234888+clarissastaudt@users.noreply.github.com>
2018-12-03 12:16:14 +0100

    remove unnecessary comments

--- main.js
+++ main.js
@@ -3,10 +3,7 @@ const electron = require('electron')
 const { app, BrowserWindow } = require('electron')
 
 function createWindow () {
-  // Erstelle das Browser-Fenster.
   win = new BrowserWindow({ width: 800, height: 600 })
-
-  // und Laden der index.html der App.
   win.loadFile('index.html')
 }
 
